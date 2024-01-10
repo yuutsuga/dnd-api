91de5fd5ce2e07ebeb1f08cdc4e784a2f46fe73f
SolucoGabriel <gabrielfagundessilveira@gmail.com>
2024-01-10 18:24:01 -0300

    [Create]: userRouter imported to APP.TS

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import 'dotenv/config';
 import creatorRouter from './routes/creator';
 import cardRouter from './routes/cards';
+import userRouter from './routes/user';
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 
 app.use('/creator', creatorRouter);
 app.use('/card', cardRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
